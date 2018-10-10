ea8555f5b6e68beda2f988c1d283304530b3434d
codediodeio <hello@jeffdelaney.me>
2018-10-09 19:30:27 -0700

    rm dist

--- dist/0-event-loop.js
+++
@@ -1,9 +0,0 @@
-// L1
-console.log('🥪 Synchronous 1');
-// L2
-setTimeout(function (_) { return console.log("\uD83C\uDF45 Timeout 2"); }, 0);
-// L3
-Promise.resolve().then(function (_) { return console.log('🍍 Promise 3'); });
-// L4
-console.log('🥪 Synchronous 4');
-//# sourceMappingURL=0-event-loop.js.map
--- dist/log.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logTimeout = function (n) { return console.log("\uD83C\uDF45 Timeout " + n); };
-exports.logPromise = function (n) { return console.log("\uD83C\uDF4D Promise " + n); };
-exports.logSync = function (n) { return console.log("\uD83E\uDD6A Synchronous " + n); };
-//# sourceMappingURL=log.js.map
