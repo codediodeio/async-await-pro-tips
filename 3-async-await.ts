751c0eb2a0e3acc93a6fedd8003d9593164a1e3d
codediodeio <hello@jeffdelaney.me>
2018-10-09 19:39:48 -0700

    prettier

--- 3-async-await.ts
+++ 3-async-await.ts
@@ -1,34 +1,31 @@
 // Basic
-export const getFruit = async(name) => {
-    const fruits = {
-        pineapple:  '🍍',
-        peach:      '🍑',
-        strawberry: '🍓'
-    }
+export const getFruit = async name => {
+  const fruits = {
+    pineapple: '🍍',
+    peach: '🍑',
+    strawberry: '🍓'
+  };
 
-    return fruits[name];
-}
+  return fruits[name];
+};
 
-getFruit('peach').then(console.log)
-
+getFruit('peach').then(console.log);
 
 // Async + Await
 
-export const makeSmoothie = async() => {
-    const a = await getFruit('pineapple');
-    const b = await getFruit('strawberry');
+export const makeSmoothie = async () => {
+  const a = await getFruit('pineapple');
+  const b = await getFruit('strawberry');
 
-    return [a, b];
-}
-
+  return [a, b];
+};
 
 const makeSmoothie2 = () => {
-    let a;
-    return getFruit('pineapple')
-        .then(v => {
-            v = a;
-            return getFruit('strawberry')
-        })
-        .then(v => [v, a])
-}
-
+  let a;
+  return getFruit('pineapple')
+    .then(v => {
+      v = a;
+      return getFruit('strawberry');
+    })
+    .then(v => [v, a]);
+};
